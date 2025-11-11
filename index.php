0a157a35154e8e60450620f113d6a4bf72dd09ed
GustavoK1510 <gustavoklc1510@gmail.com>
2025-11-11 10:08:25 -0300

    php

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    Minha Página HTML
+    <?php
+        echo "<BR>bagre beta<BR>";
+        $nome = 'gustavo goat';
+        echo $nome."<BR>";
+    ?>
+</body>
+</html>
